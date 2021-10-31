d9dab3d98e7a24db4c4dee1f732f3f9aa0785bef
stefanConstandache <stefan.m.constandache@gmail.com>
2021-10-31 18:51:15 +0200

    Resolved missing dependencies

--- qresent/lib/main.dart
+++ qresent/lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
