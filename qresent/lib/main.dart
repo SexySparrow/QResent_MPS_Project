70cda972e71d1c68348a00c533a85dc9c895cf33
Sparrow <adrianbrabete2014@gmail.com>
2021-11-08 21:41:01 +0200

    Squashed commit of the following:
    commit 2dae5908a3b2faea032b3543ce1b4c2a38652848
    Author: Sparrow <adrianbrabete2014@gmail.com>
    Date:   Mon Nov 8 21:38:31 2021 +0200
    
        Revert "Merge pull request #34 from SexySparrow/#11_add_login_for_users"
    
        This reverts commit 2593a595c859b00d3a6b5e6ca861d3065bba23c2, reversing
        changes made to 7c0bb6d43d7e4fd940c53d2a2c4a08a8722634b3.
    
    commit 2593a595c859b00d3a6b5e6ca861d3065bba23c2
    Merge: 7c0bb6d 9c95492
    Author: stefanConstandache <79802742+stefanConstandache@users.noreply.github.com>
    Date:   Mon Nov 8 21:34:59 2021 +0200
    
        Merge pull request #34 from SexySparrow/#11_add_login_for_users
    
        #11 add login for users

--- qresent/lib/main.dart
+++ qresent/lib/main.dart
@@ -1,221 +1,117 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'student_dashboard.dart';
-import 'model/user_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  final _auth = FirebaseAuth.instance;
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          "QResent",
-        ),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 60.0,
-                bottom: 30.0,
-              ),
-              child: Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.asset(
-                    'assets/images/img_login.png',
-                    height: 200,
-                    width: 200,
-                  ),
-                ),
-              ),
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 30.0,
-                      bottom: 30.0,
-                      left: 50.0,
-                      right: 50.0,
-                    ),
-                    child: TextFormField(
-                      controller: emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.email),
-                        contentPadding:
-                            const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                        hintText: "Email",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      validator: (input) {
-                        if (input!.isEmpty) {
-                          return 'Please enter an email';
-                        }
-
-                        if (!RegExp("^[a-zA-Z0-9++.-]+@[a-zA-Z0-9.-]+.[a-z].")
-                            .hasMatch(input)) {
-                          return 'Please enter a valid email';
-                        }
-
-                        return null;
-                      },
-                      textInputAction: TextInputAction.next,
-                      onSaved: (input) => emailController.text = input!,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      bottom: 30.0,
-                      left: 50.0,
-                      right: 50.0,
-                    ),
-                    child: TextFormField(
-                      controller: passwordController,
-                      textInputAction: TextInputAction.done,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.vpn_key),
-                        contentPadding:
-                            const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                        hintText: "Password",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      validator: (input) {
-                        if (input!.isEmpty) {
-                          return 'Please Enter Your Password';
-                        }
-                      },
-                      onSaved: (input) => passwordController.text = input!,
-                      obscureText: true,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: const Text(
-                'Forgot Password',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 15,
-                ),
-              ),
-            ),
-            Material(
-              elevation: 5,
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.blueAccent,
-              child: MaterialButton(
-                onPressed: () {
-                  signIn(emailController.text, passwordController.text);
-                },
-                padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                minWidth: 300,
-                child: const Text(
-                  "Login",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  Future<void> signIn(String email, String password) async {
-    final formState = _formKey.currentState;
-
-    if (formState!.validate()) {
-      formState.save();
-      await _auth
-          .signInWithEmailAndPassword(email: email, password: password)
-          .then((uid) {
-        User? user = FirebaseAuth.instance.currentUser;
-        UserModel currentUser = UserModel();
-
-        FirebaseFirestore.instance
-            .collection("Users")
-            .doc(user!.uid)
-            .get()
-            .then((DocumentSnapshot ds) {
-          currentUser = UserModel.fromMap(ds.data());
-
-          if (currentUser.accessLevel == '0') {
-            Navigator.of(context).pushReplacement(MaterialPageRoute(
-                builder: (context) => const StudentDashboard()));
-            Fluttertoast.showToast(msg: "Login Successful");
-          } else if (currentUser.accessLevel == '1') {
-            Navigator.of(context).pushReplacement(
-                // De schimbat StudentDashboard cu clasa TeacherDashboard cand aceasta o sa fie creata
-                MaterialPageRoute(
-                    builder: (context) => const StudentDashboard()));
-            Fluttertoast.showToast(msg: "Login Successful");
-          } else if (currentUser.accessLevel == '2') {
-            Navigator.of(context).pushReplacement(
-                // De schimbat StudentDashboard cu clasa AdminDashboard cand aceasta o sa fie creata
-                MaterialPageRoute(
-                    builder: (context) => const StudentDashboard()));
-            Fluttertoast.showToast(msg: "Login Successful");
-          }
-        });
-      }).catchError((e) {
-        Fluttertoast.showToast(msg: e!.message);
-      });
-    }
   }
 }
