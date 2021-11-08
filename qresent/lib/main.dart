f0bd8e44ab75afcb088647d1424757d28c399a07
stefanConstandache <stefan.m.constandache@gmail.com>
2021-11-09 00:37:03 +0200

    Fixed imports

--- qresent/lib/main.dart
+++ qresent/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'student_dashboard.dart';
