1e3d8a005e1ef0a246246e7775bcd306b0e53f2a
andreea-pantelimon <62548435+andreea-pantelimon@users.noreply.github.com>
2021-11-16 16:39:45 +0200

    Dashboard

--- qresent/lib/screens/student_profile.dart
+++ qresent/lib/screens/student_profile.dart
@@ -1,8 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -11,18 +7,10 @@ class Profile extends StatefulWidget {
   _Profile createState() => _Profile();
 }
 
-// TO DO
-// EXTRACTC INFO ABOUT USER FROM FIREBASE DATABASE
-// EXTRACT NAME, EMAIL
-// CREATE THE PROFILE PAGE
 class _Profile extends State<Profile> {
   final fullNameController = TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
   late String userEmail;
 
-  /*void getCurrentUserEmail() async {
-    final user = await _firebaseAuth.currentUser().then((value) => userEmail = value.email);
-  }*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
